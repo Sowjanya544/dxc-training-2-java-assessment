b95d4bf6d5de3139e8cccf4576dfb7d518e4c689
Sowjanya544 <96856669+Sowjanya544@users.noreply.github.com>
2022-01-12 15:10:43 +0530

    Add files via upload

---
+++ Monster.java
@@ -0,0 +1,61 @@
+public class Car 
+{
+    public void m1() 
+    {
+        System.out.println("car 1");
+    }
+
+    public void m2() 
+    {
+        System.out.println("car 2");
+    }
+
+    public String toString() 
+    {
+        return "vroom";
+    }
+}
+
+public class Truck extends Car 
+{
+    public void m1() 
+    {
+        System.out.println("truck 1");
+    }
+
+    public void m2() 
+    {
+        super.m1();
+    }
+
+    public String toString() 
+    {
+        return super.toString() + super.toString();
+    }
+}
+public class MonsterTruck extends Truck 
+{
+    public void m1() 
+    {
+        System.out.println("monster 1");
+    }
+    
+    public void m2() 
+    {
+        super.m1();
+        super.m2();
+    }
+    
+    public String toString() {
+        return "monster " + super.toString();
+    }
+}
+Public class Main()
+{
+    public static void main (String[] args) {
+        MonsterTruck ob = new MonsterTruck();
+        ob.m1();
+        ob.m2();
+        ob.toString();
+    }
+}
